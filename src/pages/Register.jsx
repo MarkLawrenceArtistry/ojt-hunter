5554ea98e90606dc6c063dafd24e4406d332a6c5
Mark Lawrence <marklawrencecatubay@gmail.com>
2026-05-31 02:07:48 +0800

    feat: Students can register.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,59 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// SERVICES/API
 import { supabase } from "../services/supabase";
+import { register } from "../services/users";
 
 export default function Register() {
-    useEffect(() => {
-        
-    }, [])
+    const [credentials, setCredentials] = useState({ email: '', password: '', name: '', campus_id: '', program_course: '' })
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            const response = await register(credentials)
+
+            if(response) {
+                alert('Register successful! Kindly check your email.')
+                navigate('/login')
+            }
+        } catch(err) {
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div>
+                        <h1>Register</h1>
+                        <p>Create your account.</p>
+                    </div>
+
+                    <div>
+                        <input type="email" placeholder="Email..." value={credentials.email} onChange={(e) => setCredentials({...credentials, email: e.target.value})} required />
+
+                        <input type="password" placeholder="Password..." value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})} required />
+
+                        <input type="text" placeholder="Name..." value={credentials.name} onChange={(e) => setCredentials({...credentials, name: e.target.value})} required />
+
+                        <input type="number" placeholder="Campus ID: 1 - CALOOCAN, 2 - VALENZUELA" value={credentials.campus_id} onChange={(e) => setCredentials({...credentials, campus_id: e.target.value})} required />
+
+                        <input type="text" placeholder="Program/Course..." value={credentials.program_course} onChange={(e) => setCredentials({...credentials, program_course: e.target.value})} required />
+                    </div>
+
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
---
+++ src/services/users.js
@@ -0,0 +1,28 @@
+import { supabase } from "./supabase";
+
+
+
+export const register = async (credentials) => {
+
+    // campus_id: 1 - CALOOCAN, 2 - VALENZUELA
+    const { data, error } = await supabase.auth.signUp({
+        email: credentials.email,
+        password: credentials.password,
+        options: {
+            data: {
+                name: credentials.name,
+                campus_id: credentials.campus_id,
+                program_course: credentials.program_course
+            }
+        }
+    })
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log(data)
+    return data
+
+}
