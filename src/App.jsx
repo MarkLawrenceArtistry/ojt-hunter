f888d1244aaf7fa007d8141eba1e9cd8fa28f005
Mark Lawrence <marklawrencecatubay@gmail.com>
2026-06-28 18:09:15 +0800

    feat: Added 404 page but havent styled it yet.

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<Login/>} />
                     <Route path='/register' element={<Register/>} />
+                    <Route path='*' element={<NotFound/>} />
 
                     <Route path='/dashboard' element={
                         <ProtectedRoute allowedRoles={['student']}>
